97bf7ca209c42e34c6f631ba95894b2dcc5e1bd9
Stefano Crespi <Stefano.Crespi@accuity.com>
2016-03-04 15:20:09 -0600

    Week 2.

--- image_share/image_share.js
+++ image_share/image_share.js
@@ -1,28 +1,42 @@
-if (Meteor.isClient) {
-  var img_data = [
-    {
-      img_src: 'meteor.jpg',
-      img_alt: 'a meteor'
-    },
-    {
-      img_src: 'planet.jpg',
-      img_alt: 'a planet'
-    },
-    {
-      img_src: 'blackhole.jpg',
-      img_alt: 'a blackhole'
-    }
-  ];  
+Images = new Mongo.Collection('images');
 
-  Template.images.helpers({images:img_data});
+if (Meteor.isClient) {
+  Template.images.helpers({images:Images.find({}, {sort:{createdOn:-1,rating:-1}})});
   
   Template.images.events({
 	'click .js-image': function(event) {
-	  $(event.target).css('width', '50px');
+		$(event.target).css('width', '50px');
+	},
+	'click .js-del-image': function(event) {
+		var image_id = this._id;
+		$("#"+image_id).hide('slow', function() {
+			Images.remove({"_id":image_id});
+		});
+	},
+	'click .js-rate-image': function(event) {
+		var rating = $(event.currentTarget).data("userrating");
+		var image_id = this.id;
+		Images.update({_id:image_id}, {$set: {rating:rating}})
+	},
+	'click .js-show-image-form': function(event) {
+		$("#image_add_form").modal('show');
 	}
   });
-}
 
-if (Meteor.isServer) {
-  console.log('server');
+  Template.image_add_form.events({
+	'submit .js-add-image': function(event) {
+		var img_src, img_alt;
+	  	img_src = event.target.img_src.value;
+	  	img_alt = event.target.img_alt.value;
+
+	  	Images.insert({
+	  		img_src:img_src,
+	  		img_alt:img_alt,
+	  		createdOn:new Date()
+	  	});
+	  	$("#image_add_form").modal('hide');
+
+	  	return false;
+	}
+  });
 }
